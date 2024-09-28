beefce50a4c06a5d7c40bb498534ababe6c11160
pkch0102@gmail.com <pkch0102@gmail.com>
2024-09-28 21:50:06 +0900

    Appconfig를 통해 생성자 주입 구현

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,17 +1,28 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
+import hello.core.discount.DiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import jdk.jshell.Diag;
 import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService  orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder(){
