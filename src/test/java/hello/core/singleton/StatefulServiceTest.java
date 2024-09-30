5d344eaed911fa201040bbca9d06fbae6f2f3528
unknown <pkch0102@gmail.com>
2024-09-30 17:39:18 +0900

    싱글톤 방식의 주의점 해결방안

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -18,15 +18,15 @@ class StatefulServiceTest {
         StatefulService s2 = ac.getBean(StatefulService.class);
 
         //ThreadA: A사용자 10000원 주문
-        s1.order("userA", 10000);
+        int userAPrice = s1.order("userA", 10000);
         //ThreadB: B사용자 20000원 주문
-        s2.order("userB", 20000);
+        int userBPrice = s2.order("userB", 20000);
 
         //ThreadA: A사용자 주문 금액 조회
-        int price = s1.getPrice();
-        System.out.println("price = " + price);
+//        int price = s1.getPrice();
+        System.out.println("userAPrice = " + userAPrice);
 
-        assertThat(s1.getPrice()).isEqualTo(20000);
+//        assertThat(s1.getPrice()).isEqualTo(20000);
     }
 
     static  class TestConfig{
